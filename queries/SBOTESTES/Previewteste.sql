7ce0c29d2fbe05e07a68ff1441687d6c1f673659
Vanderlei Szarblewski de Souza <58185310+szarblewski@users.noreply.github.com>
2026-02-17 15:13:07 -0300

    dddd

---
+++ queries/SBOTESTES/Previewteste.sql
@@ -0,0 +1,19 @@
+/* DESCRIÇÃO:
+hsgfsdgfdsgsfdgsfdgfsdgfds
+*/
+CREATE PROCEDURE TESTEEEEEEEEEEEETTTT (
+  IN tabelateste TABLE (
+    produto INTEGER
+  ),
+  IN nome NVARCHAR(100) DEFAULT 'vanderlei',
+  IN valorunitario INTEGER
+)
+LANGUAGE SQLSCRIPT
+AS
+BEGIN
+  -- Variáveis
+  DECLARE idade VARCHAR(100);
+
+  -- Corpo da procedure
+select * from ocrd  
+END;
